c21461f50d98cced8ad812689d4fda03628e4a55
Maxim190 <vbrc1212@yandex.ru>
2020-03-27 15:04:16 +0300

    add first project structure

---
+++ app/src/main/java/com/example/networknotes/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.networknotes.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.networknotes.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val root: View = inflater.inflate(R.layout.main_fragment, container, false)
+        val recyclerView: RecyclerView = root.findViewById(R.id.recycler_view)
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
